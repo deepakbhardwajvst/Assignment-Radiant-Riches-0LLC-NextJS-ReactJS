4e397e9956b48b87508017ff05d7e932b0b67af1
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-23 23:21:09 +0530

    Implement Breadcrumb Component with Styling Introduce the 'Breadcrumb' component to display a breadcrumb trail for navigation. The component utilizes the 'BreadcrumbBox' component to render breadcrumb items dynamically. The 'Breadcrumb' component contains an array of breadcrumb items, each with a label, link, and an optional SVG path for visual representation. The items are structured to represent a hierarchical navigation path. The 'BreadcrumbBox' component receives the breadcrumb items and renders them in a visually appealing and organized manner. The styling for the breadcrumb is implemented in the accompanying 'Breadcrumb.scss' file, providing consistent spacing, colors, and SVG icon styling. This commit enhances the website's navigation by introducing an intuitive breadcrumb trail, improving user experience and aiding navigation throughout the site.

---
+++ src/HomePage/Breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,40 @@
+import BreadcrumbBox from './BreadcrumbBox';
+import "./Breadcrump.scss"
+const Breadcrumb = () => {
+    const breadcrumbItems = [
+        {
+            label: 'Home', link: '/', svgPath: (<svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 7.27749L6.18084 4.12322L0 0.968957L1.90283 0L10 4.12322L1.90283 8.24645L0 7.27749Z" fill="#5C6874" />
+            </svg>
+            )
+        },
+        {
+            label: 'Hosting for all', link: '/hosting-for-all', svgPath: (<svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 7.27749L6.18084 4.12322L0 0.968957L1.90283 0L10 4.12322L1.90283 8.24645L0 7.27749Z" fill="#5C6874" />
+            </svg>
+            )
+        },
+        {
+            label: 'Hosting', link: '/hosting', svgPath: (<svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 7.27749L6.18084 4.12322L0 0.968957L1.90283 0L10 4.12322L1.90283 8.24645L0 7.27749Z" fill="#5C6874" />
+            </svg>
+            )
+        },
+        {
+            label: 'Hosting6', link: '/hosting6', svgPath: (<svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 7.27749L6.18084 4.12322L0 0.968957L1.90283 0L10 4.12322L1.90283 8.24645L0 7.27749Z" fill="#5C6874" />
+            </svg>
+            )
+        },
+        { label: 'Hosting5' },
+    ];
+
+    return (
+        <div>
+            <BreadcrumbBox items={breadcrumbItems} />
+
+        </div>
+    );
+};
+
+export default Breadcrumb;
--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import ToolsList from "./ToolList/ToolList";
+import Breadcrumb from './Breadcrumb/Breadcrumb';
 const HomePage = () => {
     return (
-        <div className='container'>
+        <div className='container bg1'>
             <Hero />
             <ToolsList />
+            <Breadcrumb />
         </div>
     )
 }
