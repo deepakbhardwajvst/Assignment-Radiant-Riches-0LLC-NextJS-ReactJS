58b819e7dfa854d8a6343d5ef73133fcd02c5c93
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-24 00:29:11 +0530

    Create Card Component for Dynamic Content Introduce the 'Card' component designed to render dynamic content based on provided data. The component includes subcomponents such as 'CardImg', 'CardRating', and 'CardText' to handle various aspects of the card structure. The component is structured to iterate over a data array, rendering individual cards with optional content based on the existence of specific data fields. This approach ensures flexibility for displaying different types of cards with diverse content.

--- src/HomePage/Card/Card.jsx
+++ src/HomePage/Card/Card.jsx
@@ -1,11 +1,30 @@
 import CardImg from "@/Components/Cards/CardImg";
 import CardRating from "@/Components/Cards/CardRating";
 import CardText from "@/Components/Cards/CardText";
-import "./Card.scss"
-const Card = () => {
+import "./Card.scss";
+
+const Card = ({ data }) => {
     return (
-        <div><CardImg /><CardRating /><CardText /></div>
-    )
-}
+        <div>
+            {data.map((item, index) => (
+                <div key={index} className="card-container">
+                    {/* Optional chaining (?) is used to handle cases where key might be missing */}
+                    {item.image && <CardImg src={item.image} alt={item.imageHeading} />}
+                    {item.rating && <CardRating rating={item.rating} star={item.star} />}
+                    {item.heading && (
+                        <CardText
+                            heading={item.heading}
+                            highlightHeading={item.highlightHeading}
+                            highlightPara={item.highlightPara}
+                            discount={item.discount}
+                            mainPara={item.mainPara}
+                            whyWeLoveItData={item.whyWeLoveItData}
+                        />
+                    )}
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default Card;
