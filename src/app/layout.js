60e2d70946157b58d68ae1a95c6730a0386f2799
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-23 20:47:30 +0530

    Implement Header Component with Stylish Navbar Introduce a stylish 'Header' component with a responsive and aesthetically pleasing navbar. The CSS file ('Header.scss') defines the visual styling for the navbar, including background color, box shadow, padding, and max-height. The search box is designed with rounded corners, a white background, and an input field with proper styling. Navigation items are styled with appropriate margins, font weights, and color. The 'Header' component ('Header.js') utilizes the styled navbar, search box, and navigation items. The search box includes an SVG icon for a search magnifier, and the navigation items are organized in a flex container for proper alignment. The component provides a clean and visually appealing navigation experience.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/Components/Header/Header";
+import Footer from "@/Components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><Header />{children}<Footer /></body>
     </html>
   );
 }
