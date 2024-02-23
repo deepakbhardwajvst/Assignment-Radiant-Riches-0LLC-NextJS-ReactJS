6f34025537b9aab696d9efbd490f3ffe8168bdf1
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-24 00:34:54 +0530

    Implement CardText Component Introduce the 'CardText' component responsible for rendering text-related information within a card. This versatile component takes various props such as 'heading', 'highlightHeading', 'highlightPara', 'discount', 'mainPara', and 'whyWeLoveItData', enabling dynamic content display. The 'CardText' component significantly enhances the presentation of the 'Card' by providing detailed textual information. This commit marks the successful implementation of the text-related functionality, contributing to a more informative and engaging card display.

--- src/Components/Cards/CardText.jsx
+++ src/Components/Cards/CardText.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
 
-const CardText = () => {
+const CardText = ({
+  heading,
+  highlightHeading,
+  highlightPara,
+  discount,
+  mainPara,
+  whyWeLoveItData,
+}) => {
   return (
-    <div>CardText</div>
-  )
+    <div>
+      {heading && <div>Heading: {heading}</div>}
+      {highlightHeading && <div>Highlight Heading: {highlightHeading}</div>}
+      {highlightPara && <div>Highlight Paragraph: {highlightPara}</div>}
+      {discount && <div>Discount: {discount}</div>}
+      {mainPara && (
+        <div>
+          Main Paragraph:
+          <ul>
+            {mainPara.map((para, index) => (
+              <li key={index}>
+                {para.num && <span>Num: {para.num}</span>} {para.text}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {whyWeLoveItData && (
+        <div>
+          Why We Love It Data:
+          <ul>
+            {whyWeLoveItData.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default CardText
+export default CardText;
