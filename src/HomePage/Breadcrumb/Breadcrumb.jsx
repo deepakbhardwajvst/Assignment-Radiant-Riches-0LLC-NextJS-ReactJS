1a6b6507502af43aca265112dae4267c6a09b2f5
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-25 12:48:40 +0530

    done

--- src/HomePage/Breadcrumb/Breadcrumb.jsx
+++ src/HomePage/Breadcrumb/Breadcrumb.jsx
@@ -9,19 +9,19 @@ const Breadcrumb = () => {
             )
         },
         {
-            label: 'Hosting for all', link: '/hosting-for-all', svgPath: (<svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+            label: 'Hosting for all', link: '/', svgPath: (<svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0 7.27749L6.18084 4.12322L0 0.968957L1.90283 0L10 4.12322L1.90283 8.24645L0 7.27749Z" fill="#5C6874" />
             </svg>
             )
         },
         {
-            label: 'Hosting', link: '/hosting', svgPath: (<svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+            label: 'Hosting', link: '/', svgPath: (<svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0 7.27749L6.18084 4.12322L0 0.968957L1.90283 0L10 4.12322L1.90283 8.24645L0 7.27749Z" fill="#5C6874" />
             </svg>
             )
         },
         {
-            label: 'Hosting6', link: '/hosting6', svgPath: (<svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+            label: 'Hosting6', link: '/', svgPath: (<svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0 7.27749L6.18084 4.12322L0 0.968957L1.90283 0L10 4.12322L1.90283 8.24645L0 7.27749Z" fill="#5C6874" />
             </svg>
             )
