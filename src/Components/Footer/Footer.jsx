a6eb994a59d5d4c34b8fba86bd675c7192e84117
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-25 05:53:03 +0530

    Enhanced the footer component: 1. Styled the footer with a minimum height of 452px and a dark background color for better visibility and aesthetic appeal. 2. Utilized the 'Inter' font family for consistent and modern typography. 3. Implemented a responsive and organized layout using Flexbox for the footer sections. 4. Adjusted font sizes, line heights, and colors to maintain a cohesive design throughout the component. 5. Added category links under the 'Categories' section and contact information under the 'Contact' section. 6. Improved the styling of the 'footer-us' section, including a dropdown icon for a more user-friendly interface. These changes result in an improved and visually appealing footer that contributes to a unified design across the entire application.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,35 @@
-import "./Footer.scss"
+import { FaAngleDown } from "react-icons/fa6";
+
+import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
-    )
-}
+        <footer className="footer">
+            <div className="f-container container">
+                <div className="footer-box">
+                    <h3>Categories</h3>
+                    <p>Web Builder</p>
+                    <p>Hosting</p>
+                    <p>About</p><p>Data Center</p><p>Robotic-Automation</p>
+                </div>
+                <div className="footer-box">
+                    <h3>Contact</h3>
+                    <p>Contact</p>
+                    <p>Privacy Policy</p>
+                    <p>Terms Of Service</p>
+                    <p>about</p>
+                    <p>Categories</p>
+                </div>
+                <div className="footer-box">
+                    <p className='footer-us'><span className='footer-span'>United States</span> <span>
+                        <FaAngleDown className="FaAngleDown" />
+                    </span></p>
+                </div>
 
-export default Footer
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
